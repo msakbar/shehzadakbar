29ad0275d12e3bca1377e1491ff41b108a80a6bf
Shehzad Akbar <akbar.shehzad@gmail.com>
2023-02-21 00:48:18 -0500

    First commit!

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,43 @@
 import '@/styles/globals.css'
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+} from "@clerk/nextjs";
 import type { AppProps } from 'next/app'
+import { useRouter } from "next/router";
+
+//  List pages you want to be publicly accessible, or leave empty if
+//  every page requires authentication. Use this naming strategy:
+//   "/"              for pages/index.js
+//   "/foo"           for pages/foo/index.js
+//   "/foo/bar"       for pages/foo/bar.js
+//   "/foo/[...bar]"  for pages/foo/[...bar].js
+const publicPages: Array<string> = [];
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    // Get the pathname
+    const { pathname } = useRouter();
+ 
+    // Check if the current route matches a public page
+    const isPublicPage = publicPages.includes(pathname);
+  // If the current route is listed as public, render it directly
+  // Otherwise, use Clerk to require authentication
+  return (
+    <ClerkProvider {...pageProps}>
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 }
