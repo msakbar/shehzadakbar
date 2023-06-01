0f45ab997a23b2e5dc04eae8d53863de742bb9bb
Shehzad Akbar <akbar.shehzad@gmail.com>
2023-06-01 19:28:25 -0400

    remove UseEffect

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -4,13 +4,7 @@ import { useEffect } from "react";
 
 const SignInPage = () => {
     const router = useRouter();
-    useEffect(() => {
-        const { redirect_url } = router.query;
-        if (redirect_url && (redirect_url as string).startsWith('http')) {
-            router.push('/sign-in');
-            return;
-        }
-    }, [router]);
+
 
 
   return <SignIn path="/sign-in" routing="path" />;
