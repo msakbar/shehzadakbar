dff650e9c2a61f9d390cb6ff2c710fdd421ffa11
Shehzad Akbar <akbar.shehzad@gmail.com>
2023-06-01 15:34:26 -0400

    user_middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,12 @@ import type { NextRequest} from "next/server";
 import { NextResponse } from "next/server";
 
 export default withClerkMiddleware( (req: NextRequest) => {
-
+    const { userId } = getAuth(req);
+    if (!userId) {
+      const signInUrl = new URL("/sign-in", req.url);
+      signInUrl.searchParams.set('redirect_url', req.nextUrl.pathname);
+      return NextResponse.redirect(signInUrl);
+    }
     return NextResponse.next();
 });
 
