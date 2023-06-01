ca1fe42930b841b1835bb01e64772daf12589c42
Shehzad Akbar <akbar.shehzad@gmail.com>
2023-06-01 16:16:02 -0400

    iterate

--- src/pages/sign-in/[[...index]].tsx
+++ src/pages/sign-in/[[...index]].tsx
@@ -2,17 +2,6 @@ import { SignIn } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-const SignInPage = () => {
-  const router = useRouter();
-  useEffect(() => {
-    const { redirect_url } = router.query;
-    if (redirect_url && (redirect_url as string).startsWith("http")) {
-      router.push("/sign-in");
-      return;
-    }
-  }, [router]);
-
-  return <SignIn path="/sign-in" routing="path" />;
-};
-
-export default SignInPage;
+export default function Page() {
+  return <SignIn />;
+}
